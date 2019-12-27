d6623e0629c0b39f2626ee466ef93f07e33eb37c
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-12-27 18:19:29 +0100

    Split clean data into training and validation set

--- training.py
+++ training.py
@@ -2,3 +2,23 @@ import os
 import sys
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+SEED = 2000
+
+
+def load_clean_data():
+    return pd.read_csv("clean_data.csv", index_col=0, encoding="latin-1")
+
+
+def split_clean_data():
+    """
+    Split clean data into training and validation set.
+
+    Returns:
+    x_train, x_validation, y_train, y_validation
+    """
+    data = load_clean_data()
+    return train_test_split(
+        data.SentimentText, data.Sentiment, test_size=0.2, random_state=SEED
+    )
