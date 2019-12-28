44e4e3286a0d4012dd7e1d77edb06f1971410e92
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-12-28 16:40:39 +0100

    Lastly, ish

--- training.py
+++ training.py
@@ -1,3 +1,6 @@
+from keras.models import Sequential
+from keras.layers import Dense, Conv1D, GlobalMaxPooling1D
+from keras.layers.embeddings import Embedding
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 import pandas as pd
@@ -38,3 +41,37 @@ sequences = tokenizer.texts_to_sequences(x_train)
 max_length = max(len(x.split()) for x in x_train)
 # pad the sequence with the max length
 x_train_seq = pad_sequences(sequences, maxlen=max_length)
+
+
+# do same for validation
+sequences_val = tokenizer.texts_to_sequences(x_validation)
+x_val_seq = pad_sequences(sequences_val, maxlen=max_length)
+
+
+# -------------------------------------
+#
+# -------------------------------------
+
+model_cnn = Sequential()
+
+e = Embedding(100000, 100, input_length=max_length)
+model_cnn.add(e)
+model_cnn.add(
+    Conv1D(filters=100, kernel_size=2, padding="valid", activation="relu", strides=1)
+)
+model_cnn.add(GlobalMaxPooling1D())
+model_cnn.add(Dense(256, activation="relu"))
+model_cnn.add(Dense(1, activation="sigmoid"))
+model_cnn.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model_cnn.fit(
+    x_train_seq,
+    y_train,
+    validation_data=(x_val_seq, y_validation),
+    epochs=5,
+    batch_size=32,
+    verbose=2,
+)
+score, acc = model_cnn.evaluate(x_val_seq, y_validation, verbose=2, batch_size=32)
+print("score: %.2f" % (score))
+print("acc: %.2f" % (acc))
